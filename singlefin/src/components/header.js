aef4724a29696735acfcd4bfbb354e8c0332b1ce
Colin Sygiel <ctsygiel@gmail.com>
2018-12-05 23:43:20 -0800

    removed header

--- singlefin/src/components/header.js
+++ singlefin/src/components/header.js
@@ -4,19 +4,7 @@ import { StaticQuery, graphql } from 'gatsby'
 import HeaderImage from '../images/header.svg'
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    {/* <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    /> */}
+  <div>
     <HeaderImage />
   </div>
 )
