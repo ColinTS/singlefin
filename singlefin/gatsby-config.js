63c302239138d58351feeb9db948753a44471b6f
Colin Sygiel <ctsygiel@gmail.com>
2019-01-01 23:17:50 -0800

    added jss

--- singlefin/gatsby-config.js
+++ singlefin/gatsby-config.js
@@ -41,6 +41,7 @@ module.exports = {
         accessToken: `${process.env.CONTENTFUL_TOKEN}`,
       },
     },
+    `gatsby-plugin-jss`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
